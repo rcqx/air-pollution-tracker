639a39e8fda9a30a323f8020970f275d321127dc
jr-cast <castanos@pm.me>
2022-07-01 02:01:56 -0600

    Add filtering to north america region

--- src/components/DetailsNa.js
+++ src/components/DetailsNa.js
@@ -1,41 +1,51 @@
-import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { nanoid } from 'nanoid';
 import CapitalCard from './CapitalCard';
-import Footer from './Footer';
+import { fetchNa } from '../redux/countries/fetchData';
 
 const DetailsNa = () => {
-  const fetchNa = useSelector((state) => state.northAmerica);
-  const capitalProps = [];
-  const selectProps = [];
-
-  fetchNa.forEach((item) => capitalProps.push(
-    <Link key={nanoid()} to="/details_api_data">
-      <CapitalCard
-        key={nanoid()}
-        imgSrc={item.flags}
-        country={item.country}
-        capital={item.capital}
-        lat={item.latlng[0]}
-        lon={item.latlng[1]}
-      />
-    </Link>,
-  ));
-
-  fetchNa.forEach((item) => selectProps.push(
-    <option value={item.country}>{item.country}</option>
-  ));
+  const [search, setSearch] = useState('');
+  const dispatch = useDispatch();
+  const naCities = useSelector((state) => state.northAmerica);
+  useEffect(() => {
+    if (!naCities.length) {
+      dispatch(fetchNa());
+    }
+  }, [dispatch]);
+  const filteredCountries = naCities.filter(
+    (item) => item.country.toLowerCase().includes(search.toLocaleLowerCase()),
+  );
 
   return (
     <>
-      <select className='filterBox' name="countries" id="pet-select">
+      {/* <select id="mySelect" className="filterBox" name="countries" id="pet-select">
         <option value="">--Choose an option--</option>
         {selectProps}
-      </select>
-      <div id="capitals" className="capitalCont">
-        {capitalProps}
+      </select> */}
+      {/* <div id="capitals" className="capitalCont">
+      {capitalProps}
+    </div>
+    <Footer /> */}
+
+      <div className="container-input">
+        <input className="filterBox" type="text" placeholder="Search capital by country" onChange={(e) => setSearch(e.target.value)} />
       </div>
-      <Footer />
+      <div id="capitals" className="capitalCont">
+        {filteredCountries.map((item) => (
+          <Link key={nanoid()} to="/details_api_data">
+            <CapitalCard
+              key={nanoid()}
+              imgSrc={item.flags}
+              country={item.country}
+              capital={item.capital}
+              lat={item.latlng[0]}
+              lon={item.latlng[1]}
+            />
+          </Link>
+        ))}
+      </div>
     </>
   );
 };
